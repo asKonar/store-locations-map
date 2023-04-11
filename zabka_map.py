120f1bf8fd6ad0c687d6416da1893fa2ec023dfa
asKonar <130477704+asKonar@users.noreply.github.com>
2023-04-11 18:18:57 +0200

    app

---
+++ zabka_map.py
@@ -0,0 +1,34 @@
+import plotly.express as px
+import pandas as pd
+
+import dash
+from dash import dcc
+from dash import html
+
+zabka = pd.read_csv('https://raw.githubusercontent.com/asKonar/zabka-locations-map/main/zabka_map_data.csv')
+
+fig = px.scatter_mapbox(
+    zabka,
+    lat="Latitude",
+    lon="Longitude",
+    hover_name="City",
+    hover_data=["Voivodeship", "Powiat", "Street"],
+    color_discrete_sequence=["blue"],
+    zoom=6,
+)
+
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(height=900),
+fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
+# fig.update_layout(mapbox_bounds={"west": 0, "east": 0, "south": 0, "north": 0})
+
+app = dash.Dash()
+server = app.server
+app.layout = html.Div(children=[
+    html.Div(
+        dcc.Graph(figure=fig),
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server()

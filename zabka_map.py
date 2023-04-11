ce84a866abaeacb83b67fe42f9c1bf9adfe88ff5
asKonar <130477704+asKonar@users.noreply.github.com>
2023-04-11 19:07:52 +0200

    Update zabka_map.py

--- zabka_map.py
+++ zabka_map.py
@@ -2,8 +2,7 @@ import plotly.express as px
 import pandas as pd
 
 import dash
-from dash import dcc
-from dash import html
+from dash import dcc, html, Dash
 
 zabka = pd.read_csv('https://raw.githubusercontent.com/asKonar/zabka-locations-map/main/zabka_map_data.csv')
 
@@ -22,7 +21,7 @@ fig.update_layout(height=900),
 fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
 # fig.update_layout(mapbox_bounds={"west": 0, "east": 0, "south": 0, "north": 0})
 
-app = dash.Dash()
+app = Dash(__name__)
 server = app.server
 app.layout = html.Div(children=[
     html.Div(

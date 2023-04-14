da9022e7a3b37340c83e8b421df659341261b396
asKonar <130477704+asKonar@users.noreply.github.com>
2023-04-14 19:00:27 +0200

    Update zabka_map.py

--- zabka_map.py
+++ zabka_map.py
@@ -4,27 +4,62 @@ import pandas as pd
 from flask import Flask
 from dash import dcc, html, Dash
 
-zabka = pd.read_csv('https://raw.githubusercontent.com/asKonar/zabka-locations-map/main/zabka_map_data.csv')
+zabka = pd.read_csv('https://raw.githubusercontent.com/asKonar/zabka-locations-map/main/zabka_map_data.csv').sort_values(by=['Voivodeship'], ascending=True)
 
-fig = px.scatter_mapbox(
-    zabka,
-    lat="Latitude",
-    lon="Longitude",
-    hover_name="City",
-    hover_data=["Voivodeship", "Powiat", "Street"],
-    color_discrete_sequence=["blue"],
-    zoom=6,
-)
+external_stylesheets = [dbc.themes.LUX]
 
-fig.update_layout(mapbox_style="open-street-map")
-fig.update_layout(height=900),
-fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
-# fig.update_layout(mapbox_bounds={"west": 0, "east": 0, "south": 0, "north": 0})
-
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.layout = html.Div(children=[
-    html.Div(
-        dcc.Graph(figure=fig),
-    )
+
+app.layout = html.Div([
+    dbc.Container([
+        html.Div([
+            dbc.Col(html.H1('Żabka location map'))
+        ]),
+        dcc.Dropdown(
+            options=zabka['Voivodeship'].unique(),
+            value='All',
+            id='voivodeship_list',
+            clearable=False
+        ),
+        html.Br(),
+        html.Div([
+            dcc.Graph(id='graph'),
+        ])
+    ])
 ])
+
+
+@app.callback(
+    Output(component_id='graph', component_property='figure'),
+    Input(component_id='voivodeship_list', component_property='value')
+)
+def graph_update(voivodeship):
+    if voivodeship == "All":
+        fig = px.scatter_mapbox(
+            zabka,
+            lat="Latitude",
+            lon="Longitude",
+            hover_name="City",
+            hover_data=["Voivodeship", "Powiat", "Street"],
+            color_discrete_sequence=["blue"],
+            zoom=6,
+        )
+    else:
+        fig = px.scatter_mapbox(
+            zabka.loc[zabka['Voivodeship'] == voivodeship],
+            lat="Latitude",
+            lon="Longitude",
+            hover_name="City",
+            hover_data=["Voivodeship", "Powiat", "Street"],
+            color_discrete_sequence=["blue"],
+        )
+
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(height=800),
+    fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
+
+    return fig
+
+
+app.run_server()
